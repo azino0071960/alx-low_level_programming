acba57898a4e5b1df4c26caa5b8f31eefcccd81d
azino0071960 <azino0071960@gmail.com>
2023-02-16 20:58:15 -0800

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+
 /**
- * main - A program that prints a line using the printf function
- * Return: 0 (Success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("with proper grammar , but the outcome is a piece of art,\n")
-		return (0);
+	printf("Size of a char: %1u byte(s)\n", sizeof(char));
+	printf("Size of an int: %1u byte(s)\n", sizeof(int));
+	printf("Size of a long int: %1u byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %1u byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %1u byte(s)\n", sizeof(float));
+	return (0);
 }
