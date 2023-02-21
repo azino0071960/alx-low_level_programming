97cca0b017e05eeb45208ae43a24c2e64f708af0
azino0071960 <azino0071960@gmail.com>
2023-02-20 17:14:23 -0800

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - print a C program using printf
  *
- * Return: Always 0 (Success)
+ * Return: 0
  */
 
 int main(void)
 {
-	printf("Size of a char: %1u byte(s)\n", sizeof(char));
-	printf("Size of an int: %1u byte(s)\n", sizeof(int));
-	printf("Size of a long int: %1u byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %1u byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %1u byte(s)\n", sizeof(float));
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
