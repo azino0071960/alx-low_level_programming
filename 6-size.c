da38e6a831badba2ab7fc0fddae1a1a8868b234e
azino0071960 <azino0071960@gmail.com>
2023-02-20 18:19:10 -0800

    Task 6

--- 6-size.c
+++ 6-size.c
@@ -18,6 +18,6 @@ int main(void)
 	printf("Size of a int: %lu byte(S)\n", (unsigned long)sizeof(b));
 	printf("Size of a int: %lu byte(S)\n", (unsigned long)sizeof(b));
 	printf("Size of a long long int: %lu byte(S)\n", (unsigned long)sizeof(d));
-	printf("Size of a float: %lu byte(S)\n", (unsigned long)sizeog(f));
+	printf("Size of a float: %lu byte(S)\n", (unsigned long)sizeof(f));
 	return (0);
 }
