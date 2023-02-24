f1cd10ad6b0205b61a23ca712f8933d7c3631bc7
azino0071960 <azino0071960@gmail.com>
2023-02-24 06:17:41 -0800

    task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -6,8 +6,9 @@
  * @y: The second number to multiply with first number
  * Return: The result of multiplication between x param and y param
  */
- *
-  int mul(int x, int y)
+
+
+int mul(int x, int y)
 {
 	return (x * y);
 }
